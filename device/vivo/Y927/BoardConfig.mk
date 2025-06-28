f6cc3614fc88db66159947774f2a37bc5d7194c5
rtyutechstudio <138952886+rtyutechstudio@users.noreply.github.com>
2025-06-28 14:45:39 +0800

    Add files via upload

---
+++ device/vivo/Y927/BoardConfig.mk
@@ -0,0 +1,94 @@
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (c) 2020 Team Win Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/vivo/Y927
+
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
+TARGET_BOARD_SUFFIX := _64
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := A6020
+
+# Encryption
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_LEGACY_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE :=console=ttyHSL0,115200,n8 vmalloc=512M androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci product.version=PD1410L_A_1.14.3
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_IMAGE_NAME := image.gz
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/image.gz
+TARGET_KERNEL_SOURCE := kernel/vivo/Y927
+TARGET_KERNEL_CONFIG :=Y927_defconfig
+
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432       BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1824625664 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3112009728 BOARD_PERSISTIMAGE_PARTITION_SIZE :28835840
+BOARD_CACHEIMAGE_PARTITION_SIZE := 304672768   BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := false
+BOARD_FIRMWAREIMAGE_PARTITION_SIZE := 67108864    # 64MB (65536 * 1024)
+BOARD_SDCARDIMAGE_PARTITION_SIZE := 9771044864   
+
+# Recovery Configuration
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/twrp.fstab  	
+
+# TWRP Configuration
+TW_DEVICE_VERSION := GHR
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_HAS_EDL_MODE := true
+TW_INCLUDE_CRYPTO := true
+TW_EXCLUDE_SUPERSU := true
+TW_USE_TOOLBOX := true
+TW_NEW_ION_HEAP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
